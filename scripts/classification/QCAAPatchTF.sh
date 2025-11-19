0e73c0a6eb9a44d40c03e00448194e785d3a94f3
Vidur Sinha <vsinha1@br014.ib.bridges2.psc.edu>
2025-11-19 15:59:09 -0500

    Initial commit

---
+++ scripts/classification/QCAAPatchTF.sh
@@ -0,0 +1,136 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=QCAAPatchTF
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/EthanolConcentration/ \
+  --model_id EthanolConcentration \
+  --model $model_name$ \
+  --data UEA \
+  --e_layers 3 \
+  --batch_size 16 \
+  --d_model 2048 \
+  --d_ff 256 \
+  --top_k 3 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 50 \
+  --patience 10 \
+  --enc_in 3
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/FaceDetection/ \
+  --model_id FaceDetection \
+  --model $model_name$ \
+  --data UEA \
+  --e_layers 3 \
+  --batch_size 16 \
+  --d_model 128 \
+  --d_ff 256 \
+  --top_k 3 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 50 \
+  --patience 10 \
+  --enc_in 3
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/Handwriting/ \
+  --model_id Handwriting \
+  --model $model_name$ \
+  --data UEA \
+  --e_layers 3 \
+  --batch_size 16 \
+  --d_model 128 \
+  --d_ff 256 \
+  --top_k 3 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 50 \
+  --patience 10 \
+  --enc_in 3
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/Heartbeat/ \
+  --model_id Heartbeat \
+  --model $model_name$ \
+  --data UEA \
+  --e_layers 3 \
+  --batch_size 16 \
+  --d_model 128 \
+  --d_ff 256 \
+  --top_k 3 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 50 \
+  --patience 10 \
+  --enc_in 3
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/JapaneseVowels/ \
+  --model_id JapaneseVowels \
+  --model $model_name$ \
+  --data UEA \
+  --e_layers 3 \
+  --batch_size 16 \
+  --d_model 128 \
+  --d_ff 256 \
+  --top_k 3 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 50 \
+  --patience 10 \
+  --enc_in 3
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/SpokenArabicDigits/ \
+  --model_id SpokenArabicDigits \
+  --model $model_name$ \
+  --data UEA \
+  --e_layers 3 \
+  --batch_size 16 \
+  --d_model 128 \
+  --d_ff 256 \
+  --top_k 3 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 50 \
+  --patience 10 \
+  --enc_in 3
+
+python -u run.py \
+  --task_name classification \
+  --is_training 1 \
+  --root_path ./dataset/UWaveGestureLibrary/ \
+  --model_id UWaveGestureLibrary \
+  --model $model_name$ \
+  --data UEA \
+  --e_layers 3 \
+  --batch_size 16 \
+  --d_model 128 \
+  --d_ff 256 \
+  --top_k 3 \
+  --des 'Exp' \
+  --itr 1 \
+  --learning_rate 0.001 \
+  --train_epochs 50 \
+  --patience 10 \
+  --enc_in 3
